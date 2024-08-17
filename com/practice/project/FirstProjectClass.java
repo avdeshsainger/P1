fa54c017440e7b10014e596c11b45a4cd196afd7
Avdesh Kumar <avdesh07@outlook.kr>
2024-08-17 20:52:45 +0100

    initial commit

---
+++ com/practice/project/FirstProjectClass.java
@@ -0,0 +1,10 @@
+package com.practice.project;
+
+public class FirstProjectClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The First Line of code");
+	}
+
+}
