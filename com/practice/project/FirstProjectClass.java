80e368bf7d3442925d6603d8878982a8e39a2c74
Avdesh Kumar <avdesh07@outlook.kr>
2024-08-17 21:36:17 +0100

    Second commit

--- com/practice/project/FirstProjectClass.java
+++ com/practice/project/FirstProjectClass.java
@@ -5,6 +5,7 @@ public class FirstProjectClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("The First Line of code");
+		System.out.println("The Second Line of code");
 	}
 
 }
